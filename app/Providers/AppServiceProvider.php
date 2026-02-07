b92acfddfed7dd57cff9c46af9cefac52c9aea8f
ets2121 <easytechsolution.ets@gmail.com>
2026-02-07 18:50:30 +0800

    force use https in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if ($this->app->environment('production', 'staging')) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
